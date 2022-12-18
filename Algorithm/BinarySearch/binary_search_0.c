57f88f50673de51a362ca3a342c60f638678d0d8
leedongju <hhotdong@gmail.com>
2022-12-19 00:25:08 +0900

    Change binary search implementation

--- Algorithm/BinarySearch/binary_search_0.c
+++ Algorithm/BinarySearch/binary_search_0.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>  // bool, true, false
 
-bool binary_search_recursive(int target, int arr[], int first, int last)
+bool binary_search(int target, int arr[], int len)
 {
-    if (first > last)
-        return false;
+    int first = 0;
+    int last  = len - 1;
+    
+    while (first <= last)
+    {
+        int mid = first +  (last - first) / 2;
 
-    int mid = (first + last) / 2;
-
-    if      (target == arr[mid]) return true;
-    else if (target <  arr[mid]) return binary_search_recursive(target, arr, first  , mid - 1);
-    else                         return binary_search_recursive(target, arr, mid + 1, last);
+        if (target == arr[mid])
+            return true;
+        else if (target < arr[mid]) 
+            last = mid - 1;
+        else
+            first = mid + 1;
+    }
+    return false; 
 }
 
 int main(void)
 {
+    int arr[] = { 23, 44, 58, 79, 100 };
+    
     int num = 0;
     printf("Enter number to search: ");
     scanf("%d", &num);
     
-    int arr[] = { 23, 44, 58, 79, 100 };
-    
-    bool found = binary_search_recursive(num, arr, 0, sizeof(arr) / sizeof(arr[0]));     
+    bool found = binary_search(num, arr, sizeof(arr) / sizeof(arr[0]));     
     printf("Number(%d) %s in the array.\n", num, found ? "found" : "not found");    
 
     return 0;
--- Algorithm/BinarySearch/binary_search_1.c
+++ Algorithm/BinarySearch/binary_search_1.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>  // bool, true, false
 
-bool binary_search(int target, int arr[], int first, int last)
+bool binary_search_recursive(int target, int arr[], int first, int last)
 {
-    int mid = 0;
-    
-    while (first <= last)
-    {
-        mid = (first + last) / 2;         
+    if (first > last)
+        return false;
 
-        if (target == arr[mid])
-            return true;
-        else if (target < arr[mid]) 
-            last = mid - 1;
-        else
-            first = mid + 1;
-    }
-    return false; 
+    int mid = first + (last - first) / 2;
+
+    if      (target == arr[mid]) return true;
+    else if (target <  arr[mid]) return binary_search_recursive(target, arr, first  , mid - 1);
+    else                         return binary_search_recursive(target, arr, mid + 1, last);
 }
 
 int main(void)
 {
+    int arr[] = { 23, 44, 58, 79, 100 };
+    
     int num = 0;
     printf("Enter number to search: ");
     scanf("%d", &num);
     
-    int arr[] = { 23, 44, 58, 79, 100 };
-    
-    bool found = binary_search(num, arr, 0, sizeof(arr) / sizeof(arr[0]));     
+    bool found = binary_search_recursive(num, arr, 0, sizeof(arr) / sizeof(arr[0]));     
     printf("Number(%d) %s in the array.\n", num, found ? "found" : "not found");    
 
     return 0;
