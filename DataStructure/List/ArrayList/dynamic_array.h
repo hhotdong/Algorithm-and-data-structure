ecdb245d8bc98b68505690e3ab892d8a2e88f6e9
leedongju <hhotdong@gmail.com>
2023-01-06 23:27:30 +0900

    Add dynamic array header file

---
+++ DataStructure/List/ArrayList/dynamic_array.h
@@ -0,0 +1,14 @@
+#ifndef _DYNAMIC_ARRAY_H_
+#define _DYNAMIC_ARRAY_H_
+
+void ListInit();
+void ListDestroy();
+void ListAdd();
+void ListAddAt();
+LData ListRemove();
+LData ListRemoveAt();
+LData ListGet();
+int ListIsEmpty();
+int ListSize();
+
+#endif  // _DYNAMIC_ARRAY_H_
