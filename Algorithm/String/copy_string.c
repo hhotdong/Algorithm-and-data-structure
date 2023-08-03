06c955d1b4daf6e909d53db24d4a6121982a860e
leedongju <hhotdong@gmail.com>
2023-08-04 00:47:55 +0900

    Add copy string algorithm

---
+++ Algorithm/String/copy_string.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void CopyString(char * from, char * to)
+{
+    int i = 0;
+    char * cur = from;
+    while (*(cur + i) != '\0')
+    {
+        *(to + i) = *(cur + i);
+        ++i;
+    }
+    *(to + i) = '\0';
+}
+
+void TestCopyString()
+{
+    char nameA[20] = "hhot";
+    char nameB[20] = { 0 };
+    
+    printf("nameA: %s\n", nameA);
+    printf("nameB: %s\n", nameB);
+
+    printf("Copy nameA to nameB.\n");
+    CopyString(nameA, nameB);
+
+    printf("nameA: %s\n", nameA);
+    printf("nameB: %s\n", nameB);
+}
+
+int main(void)
+{
+    TestCopyString();
+    return 0;
+}
