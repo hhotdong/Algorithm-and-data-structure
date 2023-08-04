94467dabc5a6cc3853a10a1d2ce88c5d602d23fd
leedongju <hhotdong@gmail.com>
2023-08-04 17:26:42 +0900

    Add cga pixel check algorithm

---
+++ Algorithm/Graphics/cga_pixel_contains_color.c
@@ -0,0 +1,73 @@
+// https://www.computerenhance.com/p/the-four-programming-questions-from?triedSigningIn=true
+// https://stackoverflow.com/questions/15114140/writing-binary-number-system-in-c-code
+// https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
+#include <stdio.h>
+
+#define B(x) S_to_binary_(#x)
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0')
+
+static inline unsigned char S_to_binary_(const char *s)
+{
+        unsigned char i = 0;
+        while (*s)
+        {
+            i <<= 1;
+            i += *s++ - '0';
+        }
+        return i;
+}
+
+int ContainsColor(unsigned char pixel, unsigned char color)
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        pixel >>= 2*i;
+        if ((pixel & 0x03) == color)
+            return 1;
+    }
+    return 0;
+}
+
+void TestColor(unsigned char pixel, unsigned char color)
+{
+    printf("Test color: pixel(" BYTE_TO_BINARY_PATTERN "), color(" BYTE_TO_BINARY_PATTERN "), result(%s)\n",
+        BYTE_TO_BINARY(pixel), BYTE_TO_BINARY(color), ContainsColor(pixel, color) ? "true" : "false");
+}
+
+int main(void)
+{
+    TestColor(B(00000000), B(00000000));
+    TestColor(B(00000000), B(00000001));
+    TestColor(B(00000000), B(00000010));
+    TestColor(B(00000000), B(00000011));
+    printf("\n");
+    
+    TestColor(B(01000000), B(00000000));
+    TestColor(B(01000000), B(00000001));
+    TestColor(B(01000000), B(00000010));
+    TestColor(B(01000000), B(00000011));
+    printf("\n");
+    
+    TestColor(B(10000000), B(00000000));
+    TestColor(B(10000000), B(00000001));
+    TestColor(B(10000000), B(00000010));
+    TestColor(B(10000000), B(00000011));
+    printf("\n");
+
+    TestColor(B(11000000), B(00000000));
+    TestColor(B(11000000), B(00000001));
+    TestColor(B(11000000), B(00000010));
+    TestColor(B(11000000), B(00000011));
+    printf("\n");
+
+    return 0;
+}
