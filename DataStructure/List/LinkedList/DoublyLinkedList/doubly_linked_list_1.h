5d5ad5ef84e28ef83f7905ec7e20ffc0b6211209
dongjulee <hhotdong@gmail.com>
2024-04-25 00:30:58 +0900

    Add linked list impl

---
+++ DataStructure/List/LinkedList/DoublyLinkedList/doubly_linked_list_1.h
@@ -0,0 +1,268 @@
+﻿// 홍정모 교수님, <자료구조 압축코스>
+#pragma once
+
+#include <cassert>
+#include <stdint.h>
+#include <algorithm>
+#include <utility>
+
+template<typename T>
+class DoublyLinkedList
+{
+public:
+	struct Node
+	{
+		T item = T();
+		Node* left = nullptr;
+		Node* right = nullptr;
+	};
+
+	DoublyLinkedList() { }
+
+    DoublyLinkedList(const DoublyLinkedList& copy)
+    {
+        Node* tmp = copy.first_;
+        while (tmp)
+        {
+            PushBack(tmp->item);
+            tmp = tmp->right;
+        }
+    }
+
+	~DoublyLinkedList()
+	{
+		Clear();
+	}
+
+	void Clear()
+	{
+        while (first_)
+            PopFront();
+	}
+
+	bool IsEmpty()
+	{
+		return first_ == nullptr;
+	}
+
+	int Size()
+	{
+		int size = 0;
+		Node* cur = first_;
+        while (cur)
+        {
+            ++size;
+            cur = cur->right;
+        }
+		return size;
+	}
+
+	void Print()
+	{
+		using namespace std;
+
+		Node* current = first_;
+
+		if (IsEmpty())
+			cout << "Empty" << endl;
+		else
+		{
+			cout << "Size = " << Size() << endl;
+
+			cout << " Forward: ";
+            Node* cur = first_;
+            while (cur->right)
+            {
+                cout << cur->item << " ";
+                cur = cur->right;
+            }
+            cout << cur->item;
+			cout << endl;
+
+            /*
+			cout << " Forward: ";
+            Node* cur = first_;
+            while (true)
+            {
+                cout << cur->item << " ";
+                if (!cur->right)
+                    break;
+                cur = cur->right;
+            }
+            */
+
+			cout << "Backward: ";
+		    while (cur->left)
+            {
+                cout << cur->item << " ";
+                cur = cur->left;
+            }
+            cout << cur->item;
+			cout << endl;
+		}
+	}
+
+	Node* Find(T item)
+	{
+        Node* cur = first_;
+        while (cur)
+        {
+            if (cur->item == item)
+                return cur;
+            cur = cur->right;
+        }
+		return nullptr;
+	}
+
+	void InsertBack(Node* node, T item)
+	{
+		if (IsEmpty())
+		{
+			PushBack(item);
+		}
+		else
+		{
+            Node* new_node = new Node;
+            new_node->item = item;
+            new_node->left = node;
+            new_node->right = node->right;
+            if (node->right)
+                node->right->left = new_node;
+            node->right = new_node;
+		}
+	}
+
+	void PushFront(T item)
+	{
+		Node* new_node = new Node;
+        new_node->item = item;
+        new_node->left = nullptr;
+        new_node->right = first_;
+        if (first_)
+            first_->left = new_node;
+        first_ = new_node;
+	}
+
+	void PushBack(T item)
+	{
+        if (IsEmpty())
+        {
+            PushFront(item);
+            return;
+        }
+        Node* cur = first_;
+        while (cur->right)
+            cur = cur->right;
+        Node* new_node = new Node;
+        new_node->item = item;
+        new_node->left = cur;
+        new_node->right = nullptr;
+        cur->right = new_node;
+	}
+
+	void PopFront()
+	{
+		if (IsEmpty())
+		{
+			using namespace std;
+			cout << "Nothing to Pop in PopFront()" << endl;
+			return;
+		}
+
+		assert(first_);
+	    Node* del_node = first_;
+        first_ = del_node->right;
+        if (first_)
+            first_->left = nullptr;
+        delete del_node;
+	}
+
+	void PopBack()
+	{
+		if (IsEmpty())
+		{
+			using namespace std;
+			cout << "Nothing to Pop in PopBack()" << endl;
+			return;
+		}
+
+		assert(first_);
+        Node* cur = first_;
+        while (cur->right)
+            cur = cur->right;
+        Node* second_last = cur->left;
+        delete cur;
+        if (second_last)
+            second_last->right = nullptr;
+        else
+            first_ = nullptr;
+
+        /*
+        assert(first_);
+        Node* cur = first_;
+        if (cur->right)
+        {
+            while (cur->right->right)
+                cur = cur->right;
+
+            Node* temp = cur->right;
+            cur->right = cur->right->right;
+
+            delete temp;
+        }
+        else
+        {
+            delete first_;
+            first_ = nullptr;
+        }
+        */
+	}
+
+	void Reverse()
+	{
+        /*
+        if (IsEmpty())
+            return;
+
+        Node* cur  = first_;
+        Node* prev = nullptr;
+
+        while (cur)
+        {
+            prev = cur;
+            cur = cur->right;
+            std::swap(prev->left, prev->right);
+        }
+
+        first_ = prev;
+        */
+
+		Node* cur = first_;
+        while (cur)
+        {
+            Node* next = cur->right;
+            std::swap(cur->left, cur->right);
+            if (next == nullptr)
+                first_ = cur;
+            cur = next;
+        }
+	}
+
+	T Front()
+	{
+		assert(first_);
+		return first_->item;
+	}
+
+	T Back()
+	{
+		assert(first_);
+        Node* cur = first_;
+        while (cur->right)
+            cur = cur->right;
+        return cur->item;
+	}
+
+protected:
+	Node* first_ = nullptr;
+};
---
+++ DataStructure/List/LinkedList/SinglyLinkedList/singly_linked_list_2.h
@@ -0,0 +1,261 @@
+﻿// 홍정모 교수님, <자료구조 압축코스>
+#pragma once
+
+#include <cassert>
+#include <stdint.h>
+
+template<typename T>
+class SinglyLinkedList
+{
+public:
+	struct Node
+	{
+		T item = T();
+		Node* next = nullptr;
+	};
+
+	SinglyLinkedList() { }
+
+	SinglyLinkedList(const SinglyLinkedList& list)
+	{
+        Node* cur = list.first_;
+        while (cur)
+        {
+            PushBack(cur->item);
+            cur = cur->next;
+        }
+	}
+
+	~SinglyLinkedList()
+	{
+		Clear();
+	}
+
+	void Clear()
+	{
+        while (first_)
+            PopFront();
+
+        /*
+        Node* del_node = first_;
+        while (del_node)
+        {
+            Node* del_node_next = del_node->next;
+            free(del_node);
+            del_node = del_node_next;
+        }
+        */
+	}
+
+	bool IsEmpty()
+	{
+		return first_ == nullptr;
+	}
+
+	int Size()
+	{
+		int size = 0;
+        Node* cur = first_;
+        while (cur)
+        {
+            ++size;
+            cur = cur->next;
+        }
+		return size;
+	}
+
+	T Front()
+	{
+		assert(first_);
+        return first_->item;
+	}
+
+	T Back()
+	{
+		assert(first_);
+        Node* cur = first_;
+        while (cur->next)
+            cur = cur->next;
+        return cur->item;
+	}
+
+	Node* Find(T item)
+	{
+        Node* cur = first_;
+
+        while (cur)
+        {
+            if (cur->item == item)
+                return cur;
+            cur = cur->next;
+        }
+        return cur;  // nullptr
+	}
+
+	void InsertBack(Node* node, T item)
+	{
+        Node* new_node = new Node;
+        new_node->item = item;
+        new_node->next = node->next;
+        node->next     = new_node;
+	}
+
+	void Remove(Node* node)
+	{
+		if (first_ == node)
+        {
+            first_ = first_->next;
+            delete node;
+            return;
+        }
+
+		Node* prev = first_;
+        while (prev->next)
+        {
+            if (prev->next == node)
+                break;
+            prev = prev->next;
+        }
+
+        // 리스트 내에 해당 노드가 존재하지 않는 경우
+        if (prev->next != node)
+            return;
+        
+        prev->next = node->next;
+        delete node;
+	}
+
+	void PushFront(T item)
+	{
+		Node* new_node = new Node;
+        new_node->item = item;
+        new_node->next = first_;
+        first_         = new_node;
+	}
+
+	void PushBack(T item)
+	{
+		if (first_)
+		{
+		    Node* new_node = new Node;
+            new_node->item = item;
+            new_node->next = nullptr;
+
+			Node* cur = first_;
+            while (cur->next)
+                cur = cur->next;
+            cur->next = new_node;
+		}
+		else
+		{
+		    Node* new_node = new Node;
+            new_node->item = item;
+            new_node->next = nullptr;
+            first_ = new_node;
+		}
+	}
+
+	void PopFront()
+	{
+		if (IsEmpty())
+		{
+			using namespace std;
+			cout << "Nothing to Pop in PopFront()" << endl;
+			return;
+		}
+
+		assert(first_);
+        Node* next = first_->next;
+        delete first_;
+        first_ = next;
+	}
+
+	void PopBack()
+	{
+		if (IsEmpty())
+		{
+			using namespace std;
+			cout << "Nothing to Pop in PopBack()" << endl;
+			return;
+		}
+
+		assert(first_);
+
+        if (first_->next == nullptr)
+        {
+            delete first_;
+            first_ = nullptr;
+            return;
+        }
+
+        Node* second_last = first_;
+        while (second_last->next->next)
+            second_last = second_last->next;
+
+        Node* temp = second_last->next;
+        second_last->next = second_last->next->next;
+        delete temp;
+	}
+
+	void Reverse()
+	{
+        Node* prev = nullptr;
+		Node* cur  = first_;
+        while (cur)
+        {
+            Node* pprev = prev;
+            prev = cur;
+            cur = cur->next;
+            prev->next = pprev;
+        }
+        first_ = prev;
+	}
+
+	void SetPrintDebug(bool flag)
+	{
+		print_debug_ = flag;
+	}
+
+	void Print()
+	{
+		using namespace std;
+
+		Node* current = first_;
+
+		if (IsEmpty())
+			cout << "Empty" << endl;
+		else
+		{
+			cout << "Size = " << Size() << " ";
+
+			while (current)
+			{
+				if (print_debug_)
+				{
+					//cout << "[" << current << ", " << current->item << ", " << current->next << "]";
+
+					// 주소를 짧은 정수로 출력 (앞 부분은 대부분 동일하기때문에 뒷부분만 출력)
+					cout << "[" << reinterpret_cast<uintptr_t>(current) % 100000 << ", "
+						<< current->item << ", "
+						<< reinterpret_cast<uintptr_t>(current->next) % 100000 << "]";
+				}
+				else
+				{
+					cout << current->item;
+				}
+
+				if (current->next)
+					cout << " -> ";
+				else
+					cout << " -> NULL";
+
+				current = current->next;
+			}
+			cout << endl;
+		}
+	}
+
+protected:
+	Node* first_ = nullptr;
+	bool print_debug_ = false;
+};
